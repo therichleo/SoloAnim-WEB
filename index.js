da65f223ebb5927dd71a3057fae3f5822be2b306
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2024-11-15 21:44:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import { engine } from 'express-handlebars'
 
-const app = express()
+const app = express();
 const PORT = 5000;
 
+//para acceder a datos de formularios dentro de la pogina
+app.use(express.urlencoded({ extended: true }));
+
+// Iniciacion de hdbs
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+
+// Ruta inicial
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(PORT,()=>{
-    console.log('Servidor con puerto ${PORT}')
-})
+app.post('/ScrappingIsBest', (req,res) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
